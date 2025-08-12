f5eb8de2bb8881b074490ba912f28a2b1fdf111c
Guilherme Ribeiro <2211005@my.ipleiria.pt>
2025-08-13 00:11:18 +0100

    User pagination and email formatting

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -8,8 +8,28 @@ const prisma = new PrismaClient();
 //Get All Users
 
 export async function getAllUsers(req:Request, res:Response){
+
+    //Se tivermos varios utilizadores, para admins sera melhor paginar os resultados
+    /**
+     * 
+     * No prisma a paginação é feita com os campos Skip e take
+     * Ou seja salta x resultados e leva outros x
+     * Aqui termos de definir a nossa pagina, o limit que será o nosso take ou seja quantos 
+     * utilizadores termos por pagina, e o skip ou seja os utilizadores da pagina anterior
+     * ou seja se tivermos na pagina 2, iremos saltar 10 utilizadores
+     * (2 -1) * 10  = 1 * 10 = 10
+     * 
+     */
+
+
+    const page = parseInt(req.query.page as string) || 1;
+    const limit = parseInt(req.query.limit as string) || 10;
+    const skip = (page - 1) * limit;
+
     try {
         const users = await prisma.user.findMany({
+            skip,
+            take: limit,
             select: {id:true, name:true,email:true,created_at:true}
         })
         res.json(users);
@@ -41,6 +61,9 @@ export async function getUserById(req: Request, res: Response) {
 // Create User
 //Testado
 export async function createUser(req:Request, res:Response) {
+
+    const expression: RegExp = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+
     //Definir os campos e retirar quais quer espaços
     const name = typeof req.body.name === "string" ? req.body.name.trim() : undefined;
     const email = typeof req.body.email === "string" ? req.body.email.trim().toLowerCase() : undefined;
@@ -50,6 +73,10 @@ export async function createUser(req:Request, res:Response) {
     if(!email || !password){
         return res.status(400).json({error: 'Email e password são obrigatorios'});
     }
+    
+    if(!expression.test(req.body.email))
+        return res.status(400).json({ error: 'O email introduzido não tem um formato válido. Por favor, verifica e tenta novamente.' });
+
 
     try {
         //10 Rondas de encriptação, normal para desenvolvimento
